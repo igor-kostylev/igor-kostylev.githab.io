b83bb0557633a5ed54f6d3aa8a55f2996d96db4f
kostylevigor <kostylev_igor@bitbucket.org>
2020-03-23 23:55:22 +0300

    fix behunt

--- behunt/js/main.js
+++ behunt/js/main.js
@@ -1,43 +1,17 @@
+$(document).ready(function(){
+
+    // slider in index.html
 $('.popular__slider').slick({
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: '<div class="prev"></div>',
-    nextArrow: '<div class="next"></div>',
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
+    prevArrow: '<div class="prev"></div>',  // arrow replacement
+    nextArrow: '<div class="next"></div>',  // arrow replacement
 });
 
-
-
+// move through the submenu with anchors
 // $('a[href*="#"]').on("click", function(e){
 //     var anchor = $(this);
 //     $('html, body').stop().animate({
@@ -47,38 +21,42 @@ $('.popular__slider').slick({
 //     return false;
 // });
 
-
+// animated disclosure user profile menu
 $('.hover_arrow').on('click', function (){
     $('.freelancers__border-user--profile').slideToggle();
 });
 
+// switched the strip and content block
 $('.profile__main-reviews--menu_item, .submenu__item').click(function() {
-    // получили дата атрибут
+    // get attribute data
     var id = $(this).attr('data-id');
 
-    // переключили полосочку
+    // switched the strip
     $('.profile__main-reviews--menu_item, .submenu__item').removeClass('active');
     $(this).addClass('active');
 
-    // переключили блок с контентом
+    // switched the content block
     $('.block').removeClass('open');
     $('.block[data-id="' + id + '"]').addClass('open');
 });
 
 $('.price__menu-item').click(function() {
-    // получили дата атрибут
+    // get attribute data
     var id = $(this).attr('data-id');
 
-    // переключили полосочку
+    // switched the strip
     $('.price__menu-item').removeClass('price__menu-item_active');
     $(this).addClass('price__menu-item_active');
 
+    // switched the content block
     $('.price__item').removeClass('price__item-open');
     $('.price__item[data-id="' + id + '"]').addClass('price__item-open');
 });
 
-var h_hght = 120; // высота шапки
-var h_mrg = 0;     // отступ когда шапка уже не видна
+
+// pin submenu
+var h_hght = 120; // submenu height
+var h_mrg = 0;     // indentation when the submenu is no longer visible
 $(function(){
     $(window).scroll(function(){
         var top = $(this).scrollTop();
@@ -90,33 +68,36 @@ $(function(){
         }
     });
 });
-$(document).ready(function(){
-    $('.slider__items').slick({
-        // autoplay: true,
-        // autoplaySpeed: 3000,
-        speed: 600,
-        infinite: true,
-        arrows: true,
-        slidesPerRow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<div class="prev"></div>',
-        nextArrow: '<div class="next"></div>',
-        asNavFor: '.slider-nav'
-    });
-    $('.slider-nav').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        speed: 600,
-        asNavFor: '.slider__items',
-        arrows: false,
-        vertical: true,
-        verticalSwiping: true,
-        focusOnSelect: true
-    });
+
+//slider in service.html
+$('.slider__items').slick({ //main slider
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 600,
+    infinite: true,
+    arrows: true,
+    slidesPerRow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<div class="prev"></div>',
+    nextArrow: '<div class="next"></div>',
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({ //side nav slider
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 600,
+    asNavFor: '.slider__items',
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true,
+    focusOnSelect: true
 });
 
+// animated block disclosure with a question
 $('.faq__bord').click(function(){
+    // get attribute data
     var id = $(this).attr('data-id');
+    $('.faq__bord_sub[data-id="' + id + '"]').slideToggle(); // animated disclosure
+});
 
-    $('.faq__bord_sub[data-id="' + id + '"]').slideToggle();
 });
